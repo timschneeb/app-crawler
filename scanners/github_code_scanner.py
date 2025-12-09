53034339e359fd5ab24b0c47957006ae3004ba12
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-12-09 06:47:29 +0100

    GH API: use pushed_at instead of updated_at

--- scanners/github_code_scanner.py
+++ scanners/github_code_scanner.py
@@ -41,7 +41,7 @@ class GithubCodeScanner(Scanner):
             try:
                 score = calc_github_score(file.repository)
                 full_results.append(App(file.repository.name, file.repository.description, [file.repository.html_url], type(self).__name__, 
-                                        score, has_github_downloads(file.repository), file.repository.updated_at))
+                                        score, has_github_downloads(file.repository), file.repository.pushed_at))
                 processed_urls.append(file.repository.html_url)
                 time.sleep(0.1)
             except RateLimitExceededException:
@@ -49,7 +49,7 @@ class GithubCodeScanner(Scanner):
                 time.sleep(1)
                 score = calc_github_score(file.repository)
                 full_results.append(App(file.repository.name, file.repository.description, [file.repository.html_url], type(self).__name__, 
-                                        score, has_github_downloads(file.repository), file.repository.updated_at))
+                                        score, has_github_downloads(file.repository), file.repository.pushed_at))
                 processed_urls.append(file.repository.html_url)
             except IndexError as e:
                 print(f'github_code: index error: {e}')

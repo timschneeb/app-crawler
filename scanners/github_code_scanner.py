3bdd010eecba22ce63331ac51f5c08d0ff0b6291
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-04-19 06:50:58 +0200

    Handle new 404 issue

--- scanners/github_code_scanner.py
+++ scanners/github_code_scanner.py
@@ -20,13 +20,28 @@ class GithubCodeScanner(Scanner):
 
         processed_urls = []
         full_results = []
-        for file in tqdm(results, total=results.totalCount):
+
+        # Manually iterate to safely catch GitHub API pagination errors
+        iterator = iter(results)
+        pbar = tqdm(total=results.totalCount)
+
+        while True:
+            try:
+                file = next(iterator)
+                pbar.update(1)
+            except StopIteration:
+                break  # Reached the end normally
+            except Exception as e:
+                # GitHub often returns 404 when you page past the *actual* results
+                print(f'\ngithub_code: API pagination stopped early (hit GitHub search limits): {e}')
+                #break
+
             if (file.repository.html_url in util.flatten([x.urls for x in self.exclude]) or
                     file.repository.html_url in processed_urls or
                     util.is_known_app(file.repository.name, [file.repository.html_url]) or
                     util.is_ignored(file.repository.name) or util.is_ignored(file.repository.html_url)):
                 continue
-                        
+
             try:
                 full_results.append(App(file.repository.name, file.repository.description, [file.repository.html_url], type(self).__name__,
                                         len(file.repository.get_releases().get_page(0)) > 0, file.repository.pushed_at))
@@ -34,5 +49,7 @@ class GithubCodeScanner(Scanner):
             except IndexError as e:
                 print(f'github_code: index error: {e}')
                 continue
+
+        pbar.close()
 
         return util.filter_known_apps(full_results, self.exclude)
